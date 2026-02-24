cc51e2ba92072651b389e7907bf99c8d04f041c3
yg1110 <younggil9488@gmail.com>
2026-02-24 18:08:10 +0900

    feat: local ip hosting

--- client/src/api/socket.ts
+++ client/src/api/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("http://192.168.0.16:3000", {
   transports: ["websocket"],
   withCredentials: true,
   autoConnect: true,
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [
     react({
       babel: {
